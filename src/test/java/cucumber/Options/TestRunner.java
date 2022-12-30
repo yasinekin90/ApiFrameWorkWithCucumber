90a0c498486d3dafa8827b0624f3c7f399dfbb23
Yasin <ekinyasin90@gmail.com>
2022-12-30 16:03:37 +0300

    placeValidations.feature file and stepdefinitions added

---
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -0,0 +1,13 @@
+package cucumber.Options;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features",
+                 glue = {"stepdefinitions"}
+)
+public class TestRunner {
+}
---
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -0,0 +1,26 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class StepDefinitions {
+
+    @Given("Add Place Payload")
+    public void add_place_payload() {
+
+    }
+    @When("user calls {string} with Post http request")
+    public void user_calls_with_post_http_request(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the API call is success with status code {int}")
+    public void the_api_call_is_success_with_status_code(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("{string} in response body is {string}")
+    public void in_response_body_is(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
